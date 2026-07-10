01514426fa2737b4fa7a8fc8d03f649185c248ed
ricorodriguez6596-boop <ricorodriguez6596@gmail.com>
2026-07-10 09:02:22 +0530

    Changes-made

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let hamburgerBtn=document.querySelector('.hamburger-btn')
 let confirmMenuContent=document.querySelector('.confirm-menu-content')
-
+let hamburgerOpen=document.querySelector('.hamburger-open')
+let hamburgerClose=document.querySelector('.hamburger-close')
 
 let extendMenu=false
 hamburgerBtn.addEventListener('click',()=>{
 
     if(extendMenu==false){
         confirmMenuContent.style.transform='translatex(0px)'
+        hamburgerOpen.style.display='none'
+        hamburgerClose.style.display='block'
         // confirmMenuContent.style.height='100vh'
     extendMenu=true
     }else{
         confirmMenuContent.style.transform='translatex(-1200px)'
+        hamburgerOpen.style.display='block'
+        hamburgerClose.style.display='none'
         // confirmMenuContent.style.height='100vh'
     extendMenu=false
     }
